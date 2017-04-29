4116de3db14c9a1854c9d11a362e8c071ada8f54
Colin Barrett <colin@springsandstruts.com>
2017-04-29 14:23:03 -0400

    Move all argument processing to args.js

--- args.js
+++ args.js
@@ -1,4 +1,21 @@
-module.exports.lex = (args) => {
+let process = require('process');
+let path = require('path');
+
+const defaults = [{ verbose: false }, { input: undefined, output: '' }]
+const argv0 = process.argv.length >= 2 ? path.basename(process.argv[1]) : process.argv0;
+
+function exitWithUsage(err) {
+  console.log('usage: ' + argv0 + ' [-v|--verbose] INPUT [OUTPUT]');
+  if (err !== undefined) {
+    console.log();
+    console.log(err);
+    process.exit(1);
+  } else {
+    process.exit();
+  }
+}
+
+function lex(argv) {
   let raw = false;
   function lexer(token) {
     let m;
@@ -11,20 +28,53 @@ module.exports.lex = (args) => {
       return { argument: token };
     }
   }
-  return args.map(lexer).filter((x) => x != null);
-};
+  return argv.map(lexer).filter((x) => x != null);
+}
 
-module.exports.parse = (lexed, defaults, options, args) => {
-  function alg(state, token) {
+function parse(lexed, defaults) {
+  function alg([opts, args], token) {
     if (token.option) {
-      state[0] = options(state[0], token.option);
+      if (/^v(erbose)?/.test(token.option)) {
+        opts.verbose = true; 
+      } else if (/^h(elp)?/.test(token.option)) {
+        exitWithUsage();
+      } else {
+        throw SyntaxError("unexpected option '"+token.option+"'.");
+      }
     } else if (token.argument) {
-      state[1] = args(state[1], token.argument);
+      args._idx = args._idx + 1 || 0;
+      switch (args._idx) {
+        case 0: args.input = path.resolve(token.argument); break;
+        case 1: args.output = path.resolve(token.argument); break;
+        default: throw SyntaxError("unexpected argument '"+token.argument+"'."); 
+      }
     } else {
       throw SyntaxError("unexpected token '"+token+"'.");
     }
-    return state;
-  };
+    return [opts, args];
+  }
   return lexed.reduce(alg, defaults || [{}, {}]);
-};
+}
 
+function read(argv, defaults) {
+  try {
+    const tokens = argv.filter((_, idx) => idx >= 2);
+    const result = parse(lex(tokens), defaults);
+    if (defaults) {
+      for (name in defaults.map(Object.keys).concat()) {
+        if (result[name] === undefined) {
+          throw SyntaxError("expected value for '"+name+"'.");
+        }
+      } 
+    }
+    return result;
+  } catch (err) {
+    if (err instanceof SyntaxError) {
+      exitWithUsage(err);
+    } else {
+      throw err;
+    }
+  }
+}
+
+module.exports = { read, defaults }
--- render.js
+++ render.js
@@ -9,58 +9,7 @@ let md = require('simple-markdown');
 
 let args = require('./args');
 
-const argv0 = process.argv.length >= 2 ? path.basename(process.argv[1]) : process.argv0;
-
-function exitWithUsage(err) {
-  console.log('usage: ' + argv0 + ' [-v|--verbose] INPUT [OUTPUT]');
-  if (err !== undefined) {
-    console.log();
-    console.log(err);
-    process.exit(1);
-  } else {
-    process.exit();
-  }
-}
-function foldOptions(opts, token) {
-  if (/^v(erbose)?/.test(token)) {
-    opts.verbose = true;    
-  } else if (/^h(elp)?/.test(token)) {
-    exitWithUsage();
-  } else {
-    throw SyntaxError("unexpected option '"+token+"'.");
-  }
-  return opts;
-}
-function foldArguments(args, token) {
-  args._idx = args._idx + 1 || 0;
-  switch (args._idx) {
-    case 0: args.input = path.resolve(token); break;
-    case 1: args.output = path.resolve(token); break;
-    default: throw SyntaxError("unexpected argument '"+token+"'."); 
-  }
-  return args;
-}
-function readInput(argv, defaults) {
-  try {
-    const tokens = argv.filter((_, idx) => idx >= 2);
-    const result = args.parse(args.lex(tokens), defaults, foldOptions, foldArguments);
-    for (name in init ? defaults.map(Object.keys).concat() : []) {
-      if (result[name] === undefined) {
-        throw SyntaxError("expected value for '"+name+"'.");
-      }
-    } 
-    return result;
-  } catch (err) {
-    if (err instanceof SyntaxError) {
-      exitWithUsage(err);
-    } else {
-      throw err;
-    }
-  }
-}
-
-let defaults = [{ verbose: false }, { input: undefined, output: '' }]
-let [{ verbose }, { input, output }] = readInput(process.argv, defaults);
+let [{ verbose }, { input, output }] = args.read(process.argv, args.defaults);
 
 if (output === '') {
   const { dir, name } = path.parse(input);
